1947bbcbc74fa01be4e27d9bd9a5cde7c5614466
NVitaly <whiskalik@gmail.com>
2022-09-07 20:08:30 +0300

    Задача 23

---
+++ task_23/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+string squareTable(int num)
+{
+    string result = $"{num} -> 1";
+
+    for (int i = 2; i <= num; i++)
+    {
+        result = result + $", {Math.Pow(i, 3)}";
+
+    }
+
+    return result;
+}
+
+try
+{
+    System.Console.Write("Введите число = ");
+    int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    System.Console.WriteLine(squareTable(num));
+
+
+}
+catch
+{
+    System.Console.WriteLine("Вводите целочисленное значение");
+}
